bfaef435d511d82dbc54b3c9b09c03e09677b002
fadynagib16009357 <fady.nagib@Student.giu-uni.de>
2025-11-12 15:59:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,20 @@
 #include <iostream>
 using namespace std;
 
-
+Animal::void display(){
+    cout<<name.get<<endl;
+    cou<<age.get<<endl;
+}
+Animal::void feed(){
+    if (ishungry)
+    cout<<no<<endl;
+    else 
+    cout<<yes<<endl;}
+enclosure :: void addAnimal(Animal*a){}
+enclosure :: displayAnimals(){
+    cout<<capacity.get<<endl;
+    cout<<currentcount<<endl;}
+}
 // ============== MAIN FUNCTION ==============
 int main() {
     
