062f5afd6fd6d7d1ad6cc5f8a51b9ce4afc75bcc
fadynagib16009357 <fady.nagib@Student.giu-uni.de>
2025-11-12 15:04:57 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -6,6 +6,22 @@ using namespace std;
 
 //define animal class here 
 
+#include <iostream>
+#include <string>
+using namespace std;
+class animal{
+    private:
+    string name;
+    int age;
+    bool ishungry;
+    public:
+    animal();
+    animal(string n,int a,bool ishug);
+    
+    ~animal();
+
+    void display();
+    void feed();    };
 
 
 #endif
