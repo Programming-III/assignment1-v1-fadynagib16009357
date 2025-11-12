de32474ce8d9f806c369f3d919522980981786a1
fadynagib16009357 <fady.nagib@Student.giu-uni.de>
2025-11-12 15:34:48 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -7,7 +7,19 @@ using namespace std;
 //define visitor class here
 
 
-
+class visitor{
+    private:
+string visitorName;
+int ticketsBought;
+public :
+visitor();
+visitor(string vN,string tB){
+visitorName=vN;
+ticketsBought=tB;
+}
+~visitor();
+void displayInfo();
+};
 
 
 
