feebd3f21b6b73f7e5918479fd66032684e6dad4
fadynagib16009357 <fady.nagib@Student.giu-uni.de>
2025-11-12 15:41:03 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -7,6 +7,21 @@
 
 
 
+class enclosure{
+    private:
+    Animal *animal;
+    int capacity;
+    int currentCount;
+    public:enclosure();
+    enclosure(Animal* a,int c,int cc){
+    animal=a;
+    capacity=c;
+    currentCount=cc;
+    }
+    ~enclosure();
+    void addAnimal(Animal* a);
+    void displayAnimals();
+};
 
 
 
